c2bd79d50edab9b48e54234d3d936dc71e9a2dfe
Harsche <henrique_harsche@hotmail.com>
2023-06-08 21:45:05 -0300

    Initial commit

---
+++ Assets/_Hub/GeneralMethods.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "General Methods", menuName = "General Methods", order = 0)]
+public class GeneralMethods : ScriptableObject{
+    public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
